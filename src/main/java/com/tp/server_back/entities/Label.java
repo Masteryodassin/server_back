0ee4a53c4545b378e976d9736a821050f14381cb
yves-mael <yves-mael.bunetel@imie.fr>
2018-03-22 16:31:36 +0100

    commit getter and setters

--- src/main/java/com/tp/server_back/entities/Label.java
+++ src/main/java/com/tp/server_back/entities/Label.java
@@ -20,4 +20,30 @@ public class Label implements Serializable {
     private List<Data> datas;
 
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Data> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<Data> datas) {
+        this.datas = datas;
+    }
+
+
 }

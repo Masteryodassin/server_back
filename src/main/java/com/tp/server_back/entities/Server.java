1f35ea66e6c41cecbb196d7a5b1159def591f1cf
Masteryodassin <yves-mael.bunetel@hotmail.fr>
2018-04-04 13:57:12 +0200

    Update Server.java

--- src/main/java/com/tp/server_back/entities/Server.java
+++ src/main/java/com/tp/server_back/entities/Server.java
@@ -14,7 +14,7 @@ public class Server implements Serializable{
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private long id;
 	@Column(name="name")
 	private String name;
 	@OneToMany(mappedBy="server",cascade = CascadeType.ALL)

df7f17944a2eb668af0a802a222e6bc279570067
yves-mael <yves-mael.bunetel@imie.fr>
2018-05-29 15:24:48 +0200

    commit implementation list labelId

--- src/main/java/com/tp/server_back/controllers/dtos/ServerDto.java
+++ src/main/java/com/tp/server_back/controllers/dtos/ServerDto.java
@@ -1,18 +1,27 @@
 package com.tp.server_back.controllers.dtos;
 
 import com.tp.server_back.entities.Server;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 public class ServerDto {
 
+
     private long id;
     private String name;
+    private List<Long> labelId = new ArrayList<Long>();
 
     public ServerDto(Server server){
         this.id = server.getId();
         this.name = server.getName();
-    }
+        this.labelId = server.getLabels().stream()
+                            .map(label -> label.getId())
+                            .collect(Collectors.toList());
 
+    }
 
 
     public long getId() {
@@ -29,5 +38,13 @@ public class ServerDto {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Long> getLabelId() {
+        return labelId;
+    }
+
+    public void setLabelId(List<Long> labelId) {
+        this.labelId = labelId;
     }
 }
